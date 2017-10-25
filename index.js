edb334dbe585142ca4527adabeca263a67aed8f7
Christian Ahn <kwanghoonahn@gmail.com>
2017-10-25 18:57:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ var height = "74"
 
 if (name === "Susan") {
   console.log("The Name is correct");
+} else {
+  console.log("Expected the name to be Susan");
 }
 
 // Don't worry about this
 module.exports = { name, height
 }
-
